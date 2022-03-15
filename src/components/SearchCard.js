af046dee22250b4e726d4eaca64c25c2e9fbec79
Kevin Whelan <kevinwhelan@pop-os.localdomain>
2022-03-15 19:26:37 -0400

    added navbar and name input fields

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -3,8 +3,15 @@ import React from 'react'
 function SearchCard(props, ref) {
   return (
     <div>
-      <input ref={ref} placeholder='State Code'></input>
-      <button onClick={() => props.getNewStateCode()}>Find</button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container justify-content-start">
+          <a className="navbar-brand" href="#">Perp-Finder</a>
+          <input type="text" placeholder='First Name'></input>
+          <input type="text" placeholder='Last Name'></input>
+          <input type="text" ref={ref} placeholder='State Code'></input>
+          <button onClick={() => props.getNewStateCode()}>Find</button>
+        </div>
+      </nav>
     </div>
   )
 }
