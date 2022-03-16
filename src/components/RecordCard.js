d9315bb52e6ae96993f3a003ccbde88ac4a5392a
Kevin Whelan <kevinwhelan@pop-os.localdomain>
2022-03-16 15:09:47 -0400

    Add bootstrap card styling to records

--- src/components/RecordCard.js
+++ src/components/RecordCard.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 function RecordCard(props) {
   return (
-    <div className="record_card">
-        <ul>
-            <li>Name: {props.data.name}</li>
-            <li>Booking Date: {props.data.book_date}</li>
-            <li>Charges:
-              <ul>
-                {props.data.charges.map(charge => <li>{charge}</li>)}
-              </ul></li>
-            <li>County: {props.data.county_state}</li>
-            <li>Mugshot: <a href={props.data.mugshot} target="_blank"><img src={props.data.mugshot} alt="mugshot" /></a></li>
-        </ul>
+    <div className='d-inline-flex flex-wrap bd-highlight mb-3 w-100'>
+      <div className='card m-3 p-3 w-40'>
+        <div className='card-body'>
+          <p className='card-title'>Name: {props.data.name}</p>
+          <p className='card-text'>Booking Date: {props.data.book_date}</p>
+          <p className='card-text'>Charges: <ul>{props.data.charges.map(charge => <li>{charge}</li>)}</ul></p>
+          <p className='card-text'>Mugshot: <a href={props.data.mugshot} target="_blank"><img src={props.data.mugshot} alt="mugshot" /></a></p>
+        </div>
+      </div>
     </div>
   )
 }
